4c8710992451720b6929b1d3a25200979b4dd7d9
Kawthar Al-Harbi <33849704+kaw18thar@users.noreply.github.com>
2018-11-26 21:47:13 +0300

    Update loganalysis.py

--- loganalysis.py
+++ loganalysis.py
@@ -7,9 +7,21 @@ def queries_SQL(question_num, query):
     c = db.cursor()
     c.execute(query)
     results = c.fetchall()
-    print("The solution for question number ", question_num, " is: ")
-    for result in results:
-        print("{}".format(result[0]), "{}".format(result[1]))
+    if question_num == 1:
+        print("\n\n1. What are the most popular three articles of all time?")
+        for result in results:
+            print("{}".format(result[0]), "{}".format(result[1]), " views")
+    elif question_num == 2:
+        print("\n\n2. Who are the most popular article authors of all time?")
+        for result in results:
+            print("{}".format(result[0]), "{}".format(result[1]), " views")
+    elif question_num == 3:
+        print('\n\n')
+        print("3. On which days did more than 1% of requests lead to errors?")
+        for result in results:
+            print("{}".format(result[1]), "{}".format(result[0]), "% errors")
+    else:
+        print("Wrong question no. please revise your code. ")
     db.close()
 
 
@@ -28,11 +40,12 @@ query_2 = '''
     WHERE articles.slug = SUBSTRING(log.path FROM 10)
     GROUP BY log.path, articles.title, articles.author
     ORDER BY views;
-    SELECT authors.name, AuthorsVa.author,
+    SELECT authors.name, AuthorsVa.views,
     SUM(views) AS totalviews
     FROM AuthorsVa, authors
     WHERE AuthorsVa.author=authors.id
-    GROUP BY AuthorsVa.author, authors.id, authors.name
+    GROUP BY AuthorsVa.author, authors.id, authors.name,
+    AuthorsVa.views
     ORDER BY totalviews DESC;
     '''
 
@@ -43,11 +56,15 @@ query_3 = '''
     COUNT(*) * 100 / SUM(COUNT(*)) OVER() AS frate
     FROM log WHERE status != '200 OK' AND DATE(time) = DATE(time)
     GROUP BY status, DATE(time) ORDER BY frate;
-    SELECT frate, days FROM fratedays
+    SELECT ROUND(frate, 2), days FROM fratedays
     WHERE frate >= 1 ORDER BY frate DESC;
     '''
 
+if __name__ == '__main__':
+    # code goes here
+    queries_SQL(1, query_1)
+    queries_SQL(2, query_2)
+    queries_SQL(3, query_3)
 
-queries_SQL(1, query_1)
-queries_SQL(2, query_2)
-queries_SQL(3, query_3)
+else:
+    print('Import')
