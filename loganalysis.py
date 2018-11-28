9fbafdf9276854beeaa5feeac6d649e80d1695ef
Kawthar Al-Harbi <33849704+kaw18thar@users.noreply.github.com>
2018-11-28 23:01:59 +0300

    Update loganalysis.py

--- loganalysis.py
+++ loganalysis.py
@@ -10,16 +10,16 @@ def queries_SQL(question_num, query):
     if question_num == 1:
         print("\n\n1. What are the most popular three articles of all time?")
         for result in results:
-            print("{article}".format(article = result[0]), "{views1}".format(views1 = result[1]), " views")
+            print("{article}".format(article = result[0]), "-- {views1}".format(views1 = result[1]), " views")
     elif question_num == 2:
         print("\n\n2. Who are the most popular article authors of all time?")
         for result in results:
-            print("{author}".format(author = result[0]), "{views2}".format(views2 = result[1]), " views")
+            print("{author} ".format(author = result[0]), "- {views2} ".format(views2 = result[1]), " views")
     elif question_num == 3:
         print('\n\n')
         print("3. On which days did more than 1% of requests lead to errors?")
         for result in results:
-            print("{day}".format(day = result[1]), "{errors}".format(errors = result[0]), "% errors")
+            print("on {day} ".format(day = result[1]), "there was {errors}".format(errors = result[0]), "% errors")
     else:
         print("Wrong question no. please revise your code. ")
     db.close()
